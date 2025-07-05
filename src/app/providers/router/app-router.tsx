5289adc98587d626febbfc20b994dc694ffe5105
Maksim <grigoriev.ma2014@yandex.ru>
2025-07-05 22:07:04 +0300

    added lazy

--- src/app/providers/router/app-router.tsx
+++ src/app/providers/router/app-router.tsx
@@ -1,15 +1,12 @@
-import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { config } from "@/app/providers/router/config.tsx";
 
 export const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Загрузка страницы ...</div>}>
-      <Routes>
-        {config.map(({ path, element }) => {
-          return <Route key={path} path={path} element={element} />;
-        })}
-      </Routes>
-    </Suspense>
+    <Routes>
+      {config.map(({ path, element }) => {
+        return <Route key={path} path={path} element={element} />;
+      })}
+    </Routes>
   );
 };
--- src/app/providers/router/config.tsx
+++ src/app/providers/router/config.tsx
@@ -1,8 +1,33 @@
 import type { RouteObject } from "react-router-dom";
-import { QuestionsList } from "@/pages/questionsList";
-import { QuestionDetails } from "@/pages/questionDetails";
+import { lazy, Suspense } from "react";
+import { Skeleton } from "@/shared/ui";
+
+const QuestionsListLazy = lazy(() =>
+  import("@/pages/questionsList").then((module) => ({
+    default: module.QuestionsList,
+  }))
+);
+export const QuestionDetailsLazy = lazy(() =>
+  import("@/pages/questionDetails").then((module) => ({
+    default: module.QuestionDetails,
+  }))
+);
 
 export const config: RouteObject[] = [
-  { path: "/", element: <QuestionsList /> },
-  { path: ":id", element: <QuestionDetails /> },
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={<Skeleton variant="list" />}>
+        <QuestionsListLazy />
+      </Suspense>
+    ),
+  },
+  {
+    path: ":id",
+    element: (
+      <Suspense fallback={<Skeleton variant="details" />}>
+        <QuestionDetailsLazy />
+      </Suspense>
+    ),
+  },
 ];
